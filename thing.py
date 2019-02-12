1a62f01926d1ed35a95ee739729fe08b6b6c157c
Sam Hird <samhird1644@gmail.com>
2019-02-12 13:49:21 +0000

    disaengagement only happens at 0.8 and above bias, virulence seems to have no effect

--- thing.py
+++ thing.py
@@ -1,10 +1,11 @@
 from random import *
 from operator import attrgetter
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+import sys
+import pylab
 
-pop_size            = 25
-para_bias           = 0.75
-host_bias           = 0.5
-virulence           = 0.75
 
 class Participant():
     """Host or Parasite parent"""
@@ -27,6 +28,8 @@ class Participant():
         self.bitList = newBitList
 
 def compete(part1, part2):
+    part1.score = 0
+    part2.score = 0
     if sum(part1.bitList) > sum(part2.bitList):
         part1.score += 1
     elif sum(part1.bitList) == sum(part2.bitList):
@@ -35,41 +38,90 @@ def compete(part1, part2):
     else: 
         part2.score += 1
 
-hostList     = []
-parasiteList = []
+hostList = []
+paraList = []
+hostResultsList = []
+paraResultsList = []
 def mainLoop(nIters=1):
-    global hostList, parasiteList
+    global hostList, paraList
     #initialise lists of participants
-    for x in range(pop_size):
-        hostList     += [Participant(host_bias)]
-        parasiteList += [Participant(para_bias)]
+    for x in range(POP_SIZE):
+        hostList     += [Participant(HOST_BIAS)]
+        paraList += [Participant(PARA_BIAS)]
 
     for iteration in range(nIters):
-        #mutate all participants
-        for host in hostList + parasiteList:
+        #mutate all participants and reset scores
+        for host in hostList:
             host.mutate()
+            host.score = 0
+        for para in paraList:
+            para.mutate()
+            para.score = 0
 
         #compete with 5 random opponents
         for host in hostList:
-            for paraIndex in sample(range(pop_size),5):
-                compete(host,parasiteList[paraIndex])
-            host.fitness = host.score                
+            for paraIndex in sample(range(POP_SIZE),5):
+                para = paraList[paraIndex]
+                if host.bitList.count(True) > para.bitList.count(True):
+                    host.score += 1
+                    #print("host")
+                elif host.bitList.count(True) == para.bitList.count(True):
+                    host.score += 0.5
+                    para.score += 0.5
+                    #print("draw")
+                else: 
+                    para.score += 1
+                    #print("para")
+            host.fitness = host.score
 
-        #calculate fitness of parasites
-        for para in parasiteList:
-            para.fitness = ((2 * para.score) / (virulence)) + ((para.score * para.score) / (virulence * virulence))
+        maxScore = max([para.score for para in paraList])
+
+        #normalise scores and calculate fitness of parasites
+        for para in paraList:
+            para.score = para.score/maxScore
+            para.fitness = ((2 * para.score) / (VIRULENCE)) + ((para.score * para.score) / (VIRULENCE * VIRULENCE))
+
+        for host in hostList:
+            hostResultsList.append([iteration,host.bitList.count(True)])
+        for para in paraList:
+            paraResultsList.append([iteration,para.bitList.count(True)])
 
         #asexual breeeding with tournement size 5
         newHostList = []
         newParaList = []
-        for index in range(pop_size):
-            newHostList.append(maxFitness(hostList[i] for i in sample(range(pop_size),5)))
-            newParaList.append(maxFitness(parasiteList[i] for i in sample(range(pop_size),5)))
-        hostList     = newHostList
-        parasiteList = newParaList
+        for index in range(POP_SIZE):
+            hostSample = [hostList[i] for i in sample(range(POP_SIZE),5)]
+            best = hostSample[0]
+            for host in hostSample:
+                best = host if host.fitness > best.fitness else best
+            newHostList.append(copy.deepcopy(best))
+
+            paraSample = [paraList[i] for i in sample(range(POP_SIZE),5)]
+            best = paraSample[0]
+            for para in paraSample:
+                best = para if para.fitness > best.fitness else best
+            newParaList.append(copy.deepcopy(best))
+        hostList = newHostList
+        paraList = newParaList
 
 def maxFitness(listParticipants):
-    best = Participant(0)
-    for part in listParticipants:
+    best = list(listParticipants)[0]
+    for part in list(listParticipants):
         best = part if part.fitness > best.fitness else best
+
     return best
+
+
+POP_SIZE            = 25
+PARA_BIAS           = 0.8
+HOST_BIAS           = 0.5
+VIRULENCE           = .5
+#randseed = randrange(sys.maxsize)
+#rng = Random(randseed)
+#print("Seed was:", randseed)
+seed(8885448394944156554)
+
+mainLoop(1000)
+plt.plot([a[0] for a in hostResultsList], [a[1] for a in hostResultsList], '.', color='red');
+plt.plot([a[0] for a in paraResultsList], [a[1] for a in paraResultsList], '.', color='blue');
+plt.show()
