aad720921c6b381d01693cbb349625cd06dbf7e1
Sam Hird <samhird1644@gmail.com>
2019-07-15 15:50:09 +0100

    resource sharing now working selection pressure still too high though

--- matchinggame.py
+++ matchinggame.py
@@ -6,18 +6,20 @@ import copy
 import sys
 import pylab
 
-BIT_LENGTH      = 100
-POP_SIZE        = 50
-HOST_BIAS       = 0.5
-PARA_BIAS       = 0.5
-MUTATE_CHANCE   = 0.003
-GENERATIONS     = 600
-USE_SEED        = None
+BIT_LENGTH       = 100
+POP_SIZE         = 50
+HOST_BIAS        = 0.5
+PARA_BIAS        = 0.5
+MUTATE_CHANCE    = 0.03
+GENERATIONS      = 600
+USE_SEED         = None
+VIRULENCE        = 1
+RESOURCE_SHARING = True
 
 class Participant():
   """Host or Parasite parent"""
-  bitList = [False] * BIT_LENGTH
-  score = 0
+  bitList = []
+  score = 0.0
   fitness = 0
   bias = 0.5
 
@@ -44,11 +46,12 @@ def mainLoop(nIters, iterOffset, hostList, paraList, virulence):
     for host in hostList:
       host.mutate()
       host.score = 0
-      host.victors = []
+      host.victorIndices = []
+      host.loserIndices = []
     for para in paraList:
       para.mutate()
       para.score = 0
-      para.victors = []
+      para.nLosses = 0
 
     #shuffle both lists and have the two populations compete pairwise
     hostWins = 0
@@ -56,7 +59,7 @@ def mainLoop(nIters, iterOffset, hostList, paraList, virulence):
       shuffle(hostList)
       for popIndex in range(POP_SIZE):
         x  = paraList[popIndex].bitList.count(True)
-        p1 = 0.5*(1+np.tanh((x-50)/7))
+        p1 = 0.5*(1.0+np.tanh((x-50)/7))
         matching = True if random()<p1 else False
         nMatching = 0
         for n in range(BIT_LENGTH):
@@ -66,25 +69,26 @@ def mainLoop(nIters, iterOffset, hostList, paraList, virulence):
         if nMatching >= 30:
           #host wins
           if RESOURCE_SHARING:
-            paraList[popIndex].victors += [hostList[popIndex]]
+            hostList[popIndex].loserIndices += [popIndex]
+            paraList[popIndex].nLosses += 1
           else:
             hostList[popIndex].score += 1
           hostWins += 1
         else:
           #para wins
           if RESOURCE_SHARING:
-            hostList[popIndex].victors += [paraList[popIndex]]
+            hostList[popIndex].victorIndices += [popIndex]
           else:
             paraList[popIndex].score += 1
 
     if RESOURCE_SHARING:
       for popIndex in range(POP_SIZE):
         #add score to paras
-        for victor in hostList[popIndex].victors:
-          victor.score += 1/len(hostList[popIndex].victors)
+        for victorIndex in hostList[popIndex].victorIndices:
+          paraList[victorIndex].score += 1.0/float(len(hostList[popIndex].victorIndices))
         #add score to hosts
-        for victor in paraList[popIndex].victors:
-          victor.score += 1/len(paraList[popIndex].victors)
+        for loserIndex in hostList[popIndex].loserIndices:
+          hostList[loserIndex].score += 1.0/paraList[loserIndex].nLosses
 
     relFitness.append([iteration,hostWins/(10*POP_SIZE)])
 
@@ -125,11 +129,10 @@ def initLists():
   hostList = []
   paraList = []
   for x in range(POP_SIZE):
-    hostList   += [Participant(HOST_BIAS)]
+    hostList += [Participant(HOST_BIAS)]
     paraList += [Participant(PARA_BIAS)]
   return (hostList, paraList)
 
-USE_SEED = 4647813385336841684
 
 if USE_SEED is None:
   USE_SEED = randrange(sys.maxsize)
@@ -137,8 +140,7 @@ if USE_SEED is None:
 print("Seed was:", USE_SEED)
 seed(USE_SEED)
 
-VIRULENCE = 1
-RESOURCE_SHARING = True
+
 
 #split virulence run
 (hostList, paraList) = initLists()
--- test.py
+++ test.py
@@ -6,26 +6,9 @@ import copy
 import sys
 import pylab
 
-class Participant():
-  """Host or Parasite parent"""
-  bitList = [False] * 100
-  score = 0
-  fitness = 0
-  bias = 0.5
-  victors = []
+someList = [1,2,3,4]
 
-  def __init__(self, bias):
-    self.bitList = []
-    for x in range(100):
-      self.bitList += [False] if random()<bias else [True]
-    self.bias = bias
+someList[2] = None
 
-part = Participant(0.5)
-
-part1 = Participant(0.75)
-
-part.victors += [part1]
-
-part.victors[0].score += 1
-
-print(part1.score)
+print( someList)
+print(len(someList))
